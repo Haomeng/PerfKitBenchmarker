e81dd0bf504922810d1a3a9feed030932b20b79b
Haomeng <whaom@cn.ibm.com>
2016-09-20 16:27:51 +0800

    Update flags.py

--- perfkitbenchmarker/providers/openstack/flags.py
+++ perfkitbenchmarker/providers/openstack/flags.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 from perfkitbenchmarker import flags
 
 flags.DEFINE_string('openstack_cli_path',
